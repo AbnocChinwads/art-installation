94c462d5949aed39d87b397445ffa7480a20a3d9
AbnocChinwads <rnemo@hotmail.co.uk>
2022-06-20 21:10:36 +0000

    updated bootstrap

--- home/models.py
+++ home/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    name = models.CharField(max_length=254)
-    description = models.TextField()
+    name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
